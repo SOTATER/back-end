bc9cfaf5d7bd979bf39950850b80479863f272a7
sooyoung_shin@tmax.co.kr <sooyoung_shin@tmax.co.kr>
2021-06-10 19:45:00 +0900

    README 수정

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # back-end
+
+### 서버 실행하기
+
+* 프로젝트 루트에서 `./gradlew bootRun` 입력
+
+### API 문서 확인하기
+
+* 서버 실행한 상태에서 브라우저를 통해 http://localhost:8080/swagger 로 접속
--- src/main/kotlin/com/sota/clone/copyopgg/web/WebConfig.kt
+++ src/main/kotlin/com/sota/clone/copyopgg/web/WebConfig.kt
@@ -2,9 +2,13 @@ package com.sota.clone.copyopgg.web
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig: WebMvcConfigurer {
 
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/swagger").setViewName("/swagger/index.html")
+    }
 }
