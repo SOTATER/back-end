522b2116997905811c809417574ea4f887822fe5
sooyoung_shin@tmax.co.kr <sooyoung_shin@tmax.co.kr>
2021-06-10 18:13:11 +0900

    init Spring Boot Project

---
+++ src/main/kotlin/com/sota/clone/copyopgg/CopyOpggApplication.kt
@@ -0,0 +1,11 @@
+package com.sota.clone.copyopgg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CopyOpggApplication
+
+fun main(args: Array<String>) {
+	runApplication<CopyOpggApplication>(*args)
+}
---
+++ src/test/kotlin/com/sota/clone/copyopgg/CopyOpggApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sota.clone.copyopgg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CopyOpggApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
