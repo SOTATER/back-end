918d359676c68e14930e0d092c5b7afe109b4f61
sooyoung_shin@tmax.co.kr <sooyoung_shin@tmax.co.kr>
2021-06-10 19:41:54 +0900

    Swagger-UI를 서버의 static 리소스로 추가

---
+++ src/main/kotlin/com/sota/clone/copyopgg/web/WebConfig.kt
@@ -0,0 +1,10 @@
+package com.sota.clone.copyopgg.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+
+}
