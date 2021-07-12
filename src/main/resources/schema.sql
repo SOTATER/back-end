d378ff1dee0d000a1318341a9e2ad47e066dba05
eddy-kor-92 <16secondlife@gmail.com>
2021-07-12 16:05:22 +0900

    code review
    - not null 명시
    - summoners_matches 테이블에서 참조할 수 있도록 id만 가진 matches 테이블
      추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,17 +9,22 @@ create table if not exists summoners (
   	primary key ("id")
 );
 
+create table if not exists matches (
+    "match_id" char(13) not null,
+    primary key("match_id")
+)
+
 create table if not exists summoners_matches (
     "match_id" char(13) references matches,
     "puuid" char(78) references summoners,
-    "spells" varchar(32)[],
-    "runes" char(4)[],
-    "kda" int[],
-    "multi_kill" int[],
-    "level" int,
-    "cs" int,
-    "dealt" int,
-    "wards" int[],
-    "items" char(4)[],
+    "spells" varchar(32)[] not null,
+    "runes" char(4)[] not null,
+    "kda" int[] not null,
+    "multi_kill" int[] not null,
+    "level" int not null,
+    "cs" int not null,
+    "dealt" int not null,
+    "wards" int[] not null,
+    "items" char(4)[] not null,
     primary key ("puuid", "match_id")
 );
