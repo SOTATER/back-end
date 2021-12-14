6ef936cb746cc29606c469fd295021e7eae26ad0
eddy-kor-92 <16secondlife@gmail.com>
2021-12-14 18:50:55 +0900

    code review
    - unused import removed

--- src/main/kotlin/com/sota/clone/copyopgg/database/jpa/JpaLeagueSummonerRepository.kt
+++ src/main/kotlin/com/sota/clone/copyopgg/database/jpa/JpaLeagueSummonerRepository.kt
@@ -2,7 +2,6 @@ package com.sota.clone.copyopgg.database.jpa
 
 import com.sota.clone.copyopgg.domain.entities.LeagueSummoner
 import com.sota.clone.copyopgg.domain.entities.LeagueSummonerPK
-import com.sota.clone.copyopgg.domain.entities.Summoner
 import com.sota.clone.copyopgg.domain.repositories.LeagueSummonerRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
