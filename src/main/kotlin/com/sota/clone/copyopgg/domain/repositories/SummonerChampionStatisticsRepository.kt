e51039416c35d3bd01e114edce6efb74e28df3fe
charlielee921 <charlielee.921@gmail.com>
2022-07-20 18:19:36 +0900

    refactor: summoner champion statistics jpa style change
    - 상속+jpaRepository 활용하지 않던 부분 활용하도록 변경

--- src/main/kotlin/com/sota/clone/copyopgg/domain/repositories/SummonerChampionStatisticsRepository.kt
+++ src/main/kotlin/com/sota/clone/copyopgg/domain/repositories/SummonerChampionStatisticsRepository.kt
@@ -5,6 +5,6 @@ import com.sota.clone.copyopgg.domain.entities.SummonerChampionStatistics
 
 interface SummonerChampionStatisticsRepository {
     fun save(summonerChampionStatistics: SummonerChampionStatistics)
+    fun saveAll(summonerChampionStatistics: List<SummonerChampionStatistics>)
     fun findByPuuidSeason(puuid: String, season: String): List<SummonerChampionStatistics>
-    fun findById(champion_id: Int, puuid: String, season: String, queue: QueueType): SummonerChampionStatistics?
 }
